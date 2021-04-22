854449e8d6790012367b49fbf32d9b8316cb6686
Lionel Humbert <helloliohumb@gmail.com>
2021-04-22 14:52:58 +0200

    Dark Trader

--- lib/01_Dark_Trader.rb
+++ lib/01_Dark_Trader.rb
@@ -1,4 +1,27 @@
+#require
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
+
+#Url
+ url = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+
+#Récuperation des cryptos en utilisant xpath et css
+ sym = url.xpath('.//tbody').css('.cmc-table__cell--sort-by__symbol')
+ sym_tx = sym.map{|abc| abc.text}
+
+#Récuperation des prix en utilisant xpath et css
+ pr = url.xpath('.//tbody').css('.cmc-table__cell--sort-by__price')
+ pr_txt = pr.map{|prc| prc.text}
+
+#Conversion de deux arrays en un hash (key = sym_tx ; value = pr_txt)
+ def perform(key, value)
+   result =[]
+   key.each_with_index do |k, v|
+     result << {k => (value)[v]}
+   end
+   puts result
+   return result
+ end
+
+ perform(sym_tx, pr_txt)
