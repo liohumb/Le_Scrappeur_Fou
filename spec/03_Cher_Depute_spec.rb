bbe259efbad5af2a5475ae7a439fc46c5b229b1c
Lionel Humbert <helloliohumb@gmail.com>
2021-04-22 22:13:09 +0200

    Cher Député

---
+++ spec/03_Cher_Depute_spec.rb
@@ -0,0 +1,8 @@
+require_relative = "../lib/03_Cher_Depute"
+
+describe "Récupération des noms et emails des députés de France" do
+  it "Récupération des noms" do
+  end
+  it "Récupération des emails" do    
+  end
+end
