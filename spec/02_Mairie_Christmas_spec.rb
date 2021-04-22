175a3e408db345fc1a74a7522c3a2afbd8c6c0dc
Lionel Humbert <helloliohumb@gmail.com>
2021-04-22 17:49:27 +0200

    Mairie Christmas

---
+++ spec/02_Mairie_Christmas_spec.rb
@@ -0,0 +1,8 @@
+require_relative = "../lib/02_Mairie_Christmas"
+
+describe "Récupération des noms de ville et des emails de mairies" do
+  it "Recupere les emails" do
+  end
+  it "Recupere la ville" do    
+  end
+end
